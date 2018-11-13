9075490cc15880fc9eba9af78693e49fa2f5177c
UK\Billy.Bellchambers <Billy.bellchambers@theaccessgroup.com>
2018-11-13 13:02:48 +0000

    Changes out tag-cards for divs and begun implementing on click

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,39 @@ import TagList from './Components/TagList';
 import logo from './logo.svg';
 import './App.css';
 
+export interface IState {
+  SelectFilter :  string;
+
+
+}
+
+export interface IUpdateState {
+  UpdateState: (stateName: string, value: string) => void;
+}
+
 class App extends React.Component {
+
+  public state = {
+    SelectFilter: '',
+  }
+
+  public UpdateState(stateName: string, value: string) {
+    this.setState({[stateName] : value});
+  }
+
 
   render() {
     return (
       <div className='vh-100 bg-light' >
         <tag-top-navbar name="Access" />
         <div className='container pt-5' >
-          <TagList ItemList={['Unit Group Selection','Area Manager','Exec Chefs','Escalation Desk','General Manager','Project Manager -Pubs']} />
+          <div className='row'>
+            <TagList UpdateState={this.UpdateState} Title='Select Filter' ItemList={['Unit Group Selection','Area Manager','Exec Chefs','Escalation Desk','General Manager','Project Manager -Pubs']} />
+            <TagList UpdateState={this.UpdateState} Title={'Select Group > '+ this.state.SelectFilter} ItemList={['Active Units','Prelauched Units','Leisure','Active & Deactivated Units FYTD','Active & Deactived Units','Example 1','Example 2', 'Example 3']} />
+            <TagList UpdateState={this.UpdateState} offset={5} Title='Selected units > ' ItemList={['0100 - Restaurant, Stevenage','1020 - Resturant, Leicester Square','1030 - Restaurant, Inverness','1060 - Restaurant, Charing Cross','1090 -  Resturant, London','Example 1','Example 2','Example 3']} />
+          </div>
         </div>
+
       </div>
     );
   }
--- src/Components/TagList.tsx
+++ src/Components/TagList.tsx
@@ -1,24 +1,39 @@
 import React, { Component } from 'react';
+import { NONAME } from 'dns';
 
 export interface IProps {
     ItemList: string[];
+    Title: string;
+    UpdateState: (stateName: string, value: string) => void;
+    offset?: number;
+    
 }
 
 const TagList = (props: IProps) => {
 
-    return (
-        <div className='col-3 text-primary'>
-            {props.ItemList.map(function(item,i) {
-               return (
+    var section: string = props.Title;
+    section = section.replace(' ','');
 
-               <tag-card version='2'>
-                    <tag-stats
-                        accent='title'
-                        sub-heading={item}
-                        label-field=''
-                        value-field=''>
-                    </tag-stats>
-                </tag-card>
+    function HandleUpdate(Section: string, Value:EventTarget ) {
+        console.log(Section);
+        console.log(Value);
+    };
+
+    return (
+        <div className='col-5 ' onClick={(e) => {HandleUpdate(section,e.target)}}>
+            <tag-text
+            type='h2'
+            text={props.Title}
+            accent='title'>
+            </tag-text>
+            {props.ItemList.map(function(item,i) {
+                var name = 'Option_' + item;
+                name = name.replace(' ','');
+               return (
+                <div key={i} id={name} className='align-right' style={{minHeight:'40px', maxHeight:'50px', padding: '0', cursor: 'pointer',  }} >
+                    {item}
+                            
+                </div>
                 )
             })}
         </div>
